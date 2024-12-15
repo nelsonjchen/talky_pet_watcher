0cf21460e6bfa6005b75e34e9d83a1d085a5034b
Nelson Chen <crazysim@gmail.com>
2024-12-15 13:27:24 -0800

    convert example to typescript

--- example-motion-listener.ts
+++ example-motion-listener.ts
@@ -1,14 +1,53 @@
-let HOSTNAME = '192.168.8.21',
-	PORT = 2020,
-	USERNAME = 'tpuser',
-	PASSWORD = 'tppass';
+let HOSTNAME: string = '192.168.8.21',
+	PORT: number = 2020,
+	USERNAME: string = 'tpuser',
+	PASSWORD: string = 'tppass';
 
 const EventMethodTypes = { PULL: "pull", SUBSCRIBE: "subscribe" }
-let EVENT_MODE = EventMethodTypes.PULL;
+let EVENT_MODE: string = EventMethodTypes.PULL;
 
-let Cam = require('onvif').Cam;
-let cam_obj = null;
+import { Cam } from 'onvif';
+let cam_obj: any = null;
 let flow = require('nimble');
+
+interface DeviceInfo {
+	manufacturer: string;
+	model: string;
+	firmwareVersion: string;
+	serialNumber: string;
+}
+
+interface SimpleItem {
+	$: {
+		Name: string;
+		Value: string;
+	}
+}
+
+interface EventMessage {
+	topic: {
+		_: string;
+	};
+	message: {
+		message: {
+			$: {
+				UtcTime: string;
+				PropertyOperation: string;
+			};
+			source?: {
+				simpleItem?: SimpleItem | SimpleItem[];
+			};
+			data?: {
+				simpleItem?: SimpleItem | SimpleItem[];
+				elementItem?: any;
+			}
+		}
+	}
+}
+
+interface EventTopic {
+	topicSet: any;
+}
 
 new Cam({
 	hostname: HOSTNAME,
@@ -17,7 +56,7 @@ new Cam({
 	port: PORT,
 	timeout: 10000,
 	preserveAddress: true
-}, function CamFunc(err) {
+}, function CamFunc(err: any) {
 	if (err) {
 		console.log(err);
 		return;
@@ -27,12 +66,12 @@ new Cam({
 
 	cam_obj = this;
 
-	let hasEvents = false;
-	let hasTopics = false;
+	let hasEvents: boolean = false;
+	let hasTopics: boolean = false;
 
 	flow.series([
-		function (callback) {
-			cam_obj.getDeviceInformation(function (err, info, xml) {
+		function (callback: any) {
+			cam_obj.getDeviceInformation(function (err: any, info: DeviceInfo, xml: any) {
 				if (!err) { console.log('Manufacturer  ' + info.manufacturer); }
 				if (!err) { console.log('Model         ' + info.model); }
 				if (!err) { console.log('Firmware      ' + info.firmwareVersion); }
@@ -40,14 +79,14 @@ new Cam({
 				callback();
 			});
 		},
-		function (callback) {
-			cam_obj.getSystemDateAndTime(function (err, date, xml) {
+		function (callback: any) {
+			cam_obj.getSystemDateAndTime(function (err: any, date: any, xml: any) {
 				if (!err) { console.log('Device Time   ' + date); }
 				callback();
 			});
 		},
-		function (callback) {
-			cam_obj.getCapabilities(function (err, data, xml) {
+		function (callback: any) {
+			cam_obj.getCapabilities(function (err: any, data: any, xml: any) {
 				if (err) {
 					console.log(err);
 				}
@@ -55,18 +94,18 @@ new Cam({
 				callback();
 			})
 		},
-		function (callback) {
+		function (callback: any) {
 			if (hasEvents) {
-				cam_obj.getEventProperties(function (err, data, xml) {
+				cam_obj.getEventProperties(function (err: any, data: EventTopic, xml: any) {
 					if (err) {
 						console.log(err);
 					} else {
-						let parseNode = function (node, topicPath) {
+						let parseNode = function (node: any, topicPath: string) {
 							for (const child in node) {
 								if (child == "$") { continue; } else if (child == "messageDescription") {
-									let IsProperty = false;
-									let source = '';
-									let data = '';
+									let IsProperty: boolean = false;
+									let source: string = '';
+									let data: string = '';
 									if (node[child].$ && node[child].$.IsProperty) { IsProperty = node[child].$.IsProperty }
 									if (node[child].source) { source = JSON.stringify(node[child].source) }
 									if (node[child].data) { data = JSON.stringify(node[child].data) }
@@ -88,9 +127,9 @@ new Cam({
 				callback()
 			}
 		},
-		function (callback) {
+		function (callback: any) {
 			if (hasEvents && hasTopics && EVENT_MODE == EventMethodTypes.PULL) {
-				cam_obj.on('event', (camMessage, xml) => {
+				cam_obj.on('event', (camMessage: EventMessage, xml: any) => {
 					ReceivedEvent(camMessage, xml);
 				})
 			}
@@ -99,11 +138,11 @@ new Cam({
 	]);
 })
 
-function stripNamespaces(topic) {
-	let output = '';
-	let parts = topic.split('/')
+function stripNamespaces(topic: string): string {
+	let output: string = '';
+	let parts: string[] = topic.split('/')
 	for (let index = 0; index < parts.length; index++) {
-		let stringNoNamespace = parts[index].split(':').pop()
+		let stringNoNamespace: string = parts[index].split(':').pop()
 		if (output.length == 0) {
 			output += stringNoNamespace
 		} else {
@@ -113,15 +152,15 @@ function stripNamespaces(topic) {
 	return output
 }
 
-function ReceivedEvent(camMessage, _xml) {
-    let eventTopic = camMessage.topic._
+function ReceivedEvent(camMessage: EventMessage, _xml: any) {
+    let eventTopic: string = camMessage.topic._
 	eventTopic = stripNamespaces(eventTopic)
 
     if (eventTopic.includes('Motion')) {
-        let eventTime = camMessage.message.message.$.UtcTime;
-        let eventProperty = camMessage.message.message.$.PropertyOperation
-        let sourceName = null
-        let sourceValue = null
+        let eventTime: string = camMessage.message.message.$.UtcTime;
+        let eventProperty: string = camMessage.message.message.$.PropertyOperation
+        let sourceName: string | null = null
+        let sourceValue: string | null = null
         if (camMessage.message.message.source && camMessage.message.message.source.simpleItem) {
             if (Array.isArray(camMessage.message.message.source.simpleItem)) {
                 sourceName = camMessage.message.message.source.simpleItem[0].$.Name
@@ -134,30 +173,30 @@ function ReceivedEvent(camMessage, _xml) {
         if (camMessage.message.message.data && camMessage.message.message.data.simpleItem) {
             if (Array.isArray(camMessage.message.message.data.simpleItem)) {
                 for (let x = 0; x < camMessage.message.message.data.simpleItem.length; x++) {
-                    let dataName = camMessage.message.message.data.simpleItem[x].$.Name
-                    let dataValue = camMessage.message.message.data.simpleItem[x].$.Value
+                    let dataName: string = camMessage.message.message.data.simpleItem[x].$.Name
+                    let dataValue: string = camMessage.message.message.data.simpleItem[x].$.Value
                     processEvent(eventTime, eventTopic, eventProperty, dataName, dataValue)
                 }
             } else {
-                let dataName = camMessage.message.message.data.simpleItem.$.Name
-                let dataValue = camMessage.message.message.data.simpleItem.$.Value
+                let dataName: string = camMessage.message.message.data.simpleItem.$.Name
+                let dataValue: string = camMessage.message.message.data.simpleItem.$.Value
                 processEvent(eventTime, eventTopic, eventProperty, dataName, dataValue)
             }
         } else if (camMessage.message.message.data && camMessage.message.message.data.elementItem) {
-            let dataName = 'elementItem'
-            let dataValue = JSON.stringify(camMessage.message.message.data.elementItem)
+            let dataName: string = 'elementItem'
+            let dataValue: string = JSON.stringify(camMessage.message.message.data.elementItem)
             processEvent(eventTime, eventTopic, eventProperty, dataName, dataValue)
         } else {
-            let dataName = null
-            let dataValue = null
+            let dataName: string | null = null
+            let dataValue: string | null = null
             processEvent(eventTime, eventTopic, eventProperty, dataName, dataValue)
         }
     }
 }
 
-function processEvent(eventTime, eventTopic, eventProperty, dataName, dataValue) {
-	let output = '';
-    const now = new Date();
+function processEvent(eventTime: string, eventTopic: string, eventProperty: string, dataName: string | null, dataValue: string | null) {
+	let output: string = '';
+    const now: Date = new Date();
 	output += `EVENT: ${now.toJSON()} ${eventTopic}`
 	if (typeof (eventProperty) !== "undefined") {
 		output += ` PROP:${eventProperty}`
