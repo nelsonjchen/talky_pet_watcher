21b7c4a047cbe5bde4c495e583188124acc30ecd
Nelson Chen <crazysim@gmail.com>
2024-12-17 20:23:27 -0800

    adze log

--- index.ts
+++ index.ts
@@ -1,28 +1,31 @@
 import config from "./config";
 import { Bot } from "grammy";
+import createLog  from "adze";
 
-console.log("Loaded config:", config);
+const log = new createLog();
+
+log.info("Loaded config:", config);
 
 const bot = new Bot(config.telegram.botToken);
 
 // TODO: Implement Telegram reporting
 async function reportToTelegram(message: string) {
-  console.log("Sending to telegram:", message);
+  log.info("Sending to telegram:", message);
   try {
     await bot.api.sendMessage(config.telegram.channelId, message);
   } catch (error) {
-    console.error("Error sending telegram message:", error);
+    log.error("Error sending telegram message:", error);
   }
 }
 
 async function main() {
-  console.log("Starting Talky Pet Watcher");
+  log.info("Starting Talky Pet Watcher");
   await reportToTelegram("Talky Pet Watcher started");
 
   config.cameras.forEach(async (camera, index) => {
-    console.log(`Camera ${index + 1}:`, camera);
+    log.info(`Camera ${index + 1}:`, camera);
     await reportToTelegram(`Camera ${index + 1} online: ${camera.ip}`);
   });
 }
 
-main().catch(console.error);
+main().catch(log.error);
